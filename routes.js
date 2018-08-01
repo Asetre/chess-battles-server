5bbcc8e5991cafd74b54653ad4c902d03856e9f6
Asetre <asetre.paul@gmail.com>
2018-07-31 18:53:50 -0700

    health check

--- routes.js
+++ routes.js
@@ -6,6 +6,9 @@ const game = require('./controller/game')
 router.use('/users', user)
 router.use('/game', game)
 
+router.get('/', (req, res) => {
+  res.status(200).res.send('online')
+})
 router.get('/health-check', (req, res) => {
   res.status(200).send('online')
 })
